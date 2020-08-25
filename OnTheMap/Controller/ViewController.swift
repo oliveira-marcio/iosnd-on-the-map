9ee79c88949b1d305637eaa8e6a60fb89b4d0955
oliveira-marcio <emaildomso@gmail.com>
2020-08-25 21:13:19 +0100

    feat: Start project

---
+++ OnTheMap/Controller/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Márcio Oliveira on 8/25/20.
+//  Copyright © 2020 Márcio Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hi")
+    }
+}
