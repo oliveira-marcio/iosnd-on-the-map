d40ab1614f19ea10d5f6eee8c38381f34b8aa24e
oliveira-marcio <emaildomso@gmail.com>
2020-08-31 12:13:50 +0100

    feat: Add CLGeocoder to find location from address

--- OnTheMap/Controller/InformationPostingViewController.swift
+++ OnTheMap/Controller/InformationPostingViewController.swift
@@ -7,24 +7,59 @@
 //
 
 import UIKit
+import CoreLocation
 
 class InformationPostingViewController: UIViewController {
+    
     @IBOutlet weak var locationTextField: UITextField!
     @IBOutlet weak var mediaURLTextField: UITextField!
+    
+    var location: String?
+    var latitude: Double?
+    var longitude: Double?
     
     @IBAction func cancelAddLocation(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func findLocation() {
-        self.performSegue(withIdentifier: "showLocationResults", sender: nil)
+        guard let address = self.locationTextField.text, !address.isEmpty else {
+            return
+        }
+        
+        CLGeocoder().geocodeAddressString(address, completionHandler: {(placemarks, error) -> Void in
+            if let placemark = placemarks?.first {
+                if let location = placemark.location {
+                    self.latitude = location.coordinate.latitude
+                    self.longitude = location.coordinate.longitude
+                    
+                    if let country = placemark.country {
+                        self.location = "\(address), \(country)"
+                    } else {
+                        self.location = address
+                    }
+                    
+                    self.performSegue(withIdentifier: "showLocationResults", sender: nil)
+                } else {
+                    self.showGeocodeFailure(message: "Couldn't fetch location coordinates. Please try again.")
+                }
+            } else {
+                self.showGeocodeFailure(message: "Couldn't find a location with provided address. Please try again.")
+            }
+        })
+    }
+    
+    func showGeocodeFailure(message: String) {
+        let alert = UIAlertController(title: "Find Location Failed", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let mapViewController = segue.destination as! InformationPostingMapViewController
-        mapViewController.location = self.locationTextField.text ?? ""
-        mapViewController.latitude = 40.6892574
-        mapViewController.longitude = -74.0446083
+        mapViewController.location = self.location!
+        mapViewController.latitude = self.latitude!
+        mapViewController.longitude = self.longitude!
         mapViewController.mediaURL = self.mediaURLTextField.text ?? ""
     }
 }
