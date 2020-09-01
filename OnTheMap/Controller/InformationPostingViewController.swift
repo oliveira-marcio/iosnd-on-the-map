01c2bf2c2eba6e0d304062fe9bd7d43399d12230
oliveira-marcio <emaildomso@gmail.com>
2020-09-01 13:56:32 +0100

    feat: Blank search field will disable find location button

--- OnTheMap/Controller/InformationPostingViewController.swift
+++ OnTheMap/Controller/InformationPostingViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreLocation
 
-class InformationPostingViewController: UIViewController {
+class InformationPostingViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var mediaURLTextField: UITextField!
@@ -23,8 +23,21 @@ class InformationPostingViewController: UIViewController {
     
     var addLocationDelegate: AddLocationDelegate?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.searchTextField.delegate = self
+        self.findLocationButton.isEnabled = false
+    }
+    
     @IBAction func cancelAddLocation(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        self.findLocationButton.isEnabled = newText.length > 0
+        return true
     }
     
     @IBAction func findLocation() {
